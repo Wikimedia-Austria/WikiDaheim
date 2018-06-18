ce8ee424ac041c140dee73158f76807248a69dd2
Benedikt Reiter <contact@benereiter.com>
2018-06-18 16:02:31 +0200

    Revert "changed API endpoint"
    This reverts commit 41d0e8b4a223fb0b32efcea7a5095dca62eecb80.

--- source/js/config/config.js
+++ source/js/config/config.js
@@ -9,4 +9,4 @@ export const LANGUAGE_CODE = 'de_AT';
 export const COUNTRY_CODE = 'at';
 export const MAPBOX_API_KEY = 'pk.eyJ1Ijoid2lraW1lZGlhYXVzdHJpYSIsImEiOiJjajUyZWw3MTMwZGVpMnJxbGsxdnN4ZnlqIn0.9D5QSHyd8fs0_Wg2zlFnQg';
 
-export const WIKIDAHEIM_ENDPOINT = 'https://api.wikidaheim.at/api.php';
+export const WIKIDAHEIM_ENDPOINT = 'https://wikidaheim.at/api/api.php';
