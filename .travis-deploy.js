7b9efa36fdf97eaab2b9290d05156367294daa41
Benedikt Reiter <contact@benereiter.com>
2018-07-02 18:19:25 +0200

    added ftp-exclude to deploy config

--- .travis-deploy.js
+++ .travis-deploy.js
@@ -3,7 +3,7 @@ var FtpDeploy = require('ftp-deploy');
 var fs = require('fs');
 var path = require('path');
 var ENV = process.env;
-var BUILD_PATH = path.resolve(__dirname, ENV.FTP_BUILD_PATH || 'build');
+var BUILD_PATH = path.resolve(__dirname, 'build');
 var TARGET_PATH = '/';
 var USERNAME = ENV.wikidaheim_ftp_user;
 var PASSWORD = ENV.wikidaheim_ftp_password;
@@ -19,6 +19,7 @@ var config = {
     localRoot: BUILD_PATH,
     remoteRoot: TARGET_PATH,
     include: ['*', '**/*', '*.htaccess'],
+    exclude: [],
     deleteRoot: false,
 }
 // use with callback
