21764ef229f8c31a1229dec20e44bf93036e4d54
Benedikt Reiter <contact@benereiter.com>
2018-07-03 19:19:36 +0200

    changed router back to browser router

--- source/js/components/Client.jsx
+++ source/js/components/Client.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import ScrollToTop from './Global/ScrollToTop';
-
-// use HashRouter for gh-pages via Travis CI
-const Router = !!process.env.CONTINUOUS_INTEGRATION ? HashRouter : BrowserRouter; // eslint-disable-line
 
 export default () => (
   <Router>
