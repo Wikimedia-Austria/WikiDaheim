dd0765fc6aed102525ec82478b4447e890c72edf
Benedikt Reiter <contact@benereiter.com>
2018-07-03 14:26:50 +0200

    IE11 fixes, retry deployment

--- .travis-deploy.js
+++ .travis-deploy.js
@@ -1,1 +1,20 @@
 // deploy.js
+var fs = require( 'vinyl-fs' );
+var ftp = require( 'vinyl-ftp' );
+var path = require('path');
+var ENV = process.env;
+var BUILD_PATH = path.resolve(__dirname, 'build');
+var TARGET_PATH = '/';
+var USERNAME = ENV.wikidaheim_ftp_user;
+var PASSWORD = ENV.wikidaheim_ftp_password;
+var HOST = ENV.wikidaheim_ftp_server;
+
+var conn = ftp.create( {
+		host:     HOST,
+		user:     USERNAME,
+		password: PASSWORD,
+		parallel: 10,
+	} );
+
+fs.src( [ `${ BUILD_PATH }/**` ], { buffer: false } )
+	.pipe( conn.dest( '/' ) );
