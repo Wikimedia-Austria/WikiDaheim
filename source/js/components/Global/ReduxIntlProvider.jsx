a5be4610d3e487ed0c63541c31faef026f247244
Benedikt Reiter <contact@benereiter.com>
2019-07-08 12:30:59 +0200

    added dependencies for italian

--- source/js/components/Global/ReduxIntlProvider.jsx
+++ source/js/components/Global/ReduxIntlProvider.jsx
@@ -4,11 +4,12 @@ import { addLocaleData, IntlProvider } from 'react-intl';
 import localeDE from 'react-intl/locale-data/de';
 import localeEN from 'react-intl/locale-data/en';
 import localeHU from 'react-intl/locale-data/hu';
+import localeIT from 'react-intl/locale-data/it';
 
 import { FALLBACK_LANGUAGE } from 'config/config';
 import * as Messages from '../../../translations/locales';
 
-addLocaleData([...localeDE, ...localeEN, ...localeHU]);
+addLocaleData([...localeDE, ...localeEN, ...localeHU, ...localeIT]);
 
 function mapStateToProps(state) {
   const language = state.locale.get('language');
