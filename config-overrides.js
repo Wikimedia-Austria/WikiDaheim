2d48b775cf19134bd625c89df373d7b48b765e70
Benedikt Reiter <contact@benereiter.com>
2023-06-14 14:50:45 +0200

    fix build errors

--- config-overrides.js
+++ config-overrides.js
@@ -1,35 +1,38 @@
-const {
-  override,
-  addWebpackModuleRule,
-} = require("customize-cra");
+const { override, addWebpackModuleRule } = require("customize-cra");
 
-const setGlobalObject = value => config => {
+const setGlobalObject = (value) => (config) => {
   // mutate config as you want
   config.output.globalObject = value;
 
   // return config so the next function in the pipeline receives it as argument
-  return config
-}
+  return config;
+};
+
+const overrideStats = (value) => (config) => {
+  config.stats = value;
+  return config;
+};
 
 module.exports = (config, env) => {
   return override(
-    setGlobalObject('(self || this)'),
+    setGlobalObject("(self || this)"),
+    /*overrideStats({
+      children: true
+    }),*/ // enable if you weant to see more detailed stats for debug
     addWebpackModuleRule({
       test: /\.html$/,
-      use: [
-        {loader: 'html-loader', options: { esModule: true }}
-      ]
+      use: [{ loader: "html-loader", options: { esModule: true } }],
     }),
     addWebpackModuleRule({
       test: /(\.worker|mapbox-gl-csp-worker).js$/,
       use: [
         {
-          loader: 'worker-loader',
+          loader: "worker-loader",
           options: {
-            filename: "[name].[contenthash].worker.js"
-          }
+            filename: "[name].[contenthash].worker.js",
+          },
         },
-      ]
+      ],
     })
   )(config, env);
-}
+};
