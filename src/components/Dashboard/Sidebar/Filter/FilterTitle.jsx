0310518201e4037693052c63769d2b7b908c09da
Benedikt Reiter <contact@benereiter.com>
2023-06-26 18:50:31 +0200

    added search filter info

--- src/components/Dashboard/Sidebar/Filter/FilterTitle.jsx
+++ src/components/Dashboard/Sidebar/Filter/FilterTitle.jsx
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { connect } from 'react-redux';
-import { toggleFilterMenu } from 'redux/actions/menu';
-import { FormattedMessage } from 'react-intl';
-import CategoryFilter from './CategoryFilter';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { connect } from "react-redux";
+import { toggleFilterMenu } from "redux/actions/menu";
+import { FormattedMessage } from "react-intl";
+import CategoryFilter from "./CategoryFilter";
 
 class FilterTitle extends Component {
   static propTypes = {
     items: PropTypes.object,
     showFilterMenu: PropTypes.bool,
     filtersActive: PropTypes.bool,
-
+    searchFilter: PropTypes.string,
     dispatch: PropTypes.func,
   };
 
@@ -28,61 +28,77 @@ class FilterTitle extends Component {
   }
 
   render() {
-    const { items, showFilterMenu, filtersActive } = this.props;
+    const { items, showFilterMenu, filtersActive, searchFilter } = this.props;
 
     const ButtonClass = classNames({
-      'FilterButton': true,
-      'FilterButton--open': showFilterMenu,
-      'FilterButton--filtersActive': filtersActive,
+      FilterButton: true,
+      "FilterButton--open": showFilterMenu,
+      "FilterButton--filtersActive": filtersActive,
     });
 
     return (
-      <header className='FilterTitle'>
-        <h2>
-          <FormattedMessage
-            id='filter.itemCount'
-            description='Title of the Filter Menu'
-            defaultMessage='{itemCount, plural,
-                =0 {keine Objekte}
-                one {1 Objekte}
-                other {{itemCount} Objekte}
-            }'
-            values={ {
-              itemCount: items ? items.size : 0,
-            } }
-          />
-        </h2>
+      <header className="FilterTitle">
+        <div className="FilterTitle-Left">
+          <h2>
+            <FormattedMessage
+              id="filter.itemCount"
+              description="Title of the Filter Menu"
+              defaultMessage="{itemCount, plural,
+                  =0 {keine Objekte}
+                  one {1 Objekte}
+                  other {{itemCount} Objekte}
+              }"
+              values={{
+                itemCount: items ? items.size : 0,
+              }}
+            />
+          </h2>
+          {searchFilter && (
+            <span>
+              <strong>
+                <FormattedMessage
+                  id="filter.searchInfo"
+                  description="Title of the Search Message"
+                  defaultMessage="Suche:"
+                />
+              </strong>{" "}
+              {searchFilter}
+            </span>
+          )}
+        </div>
 
         <div className="FilterTitle-Right">
-          <CategoryFilter asIcons={ true } />
+          <CategoryFilter asIcons={true} />
 
-          <button
-            className={ ButtonClass }
-            onClick={ this.toggle }
-          >
+          <button className={ButtonClass} onClick={this.toggle}>
             <FormattedMessage
-              id='filter.filterButtonTitle'
-              description='Title of the Filter Menu Toggle-Button'
-              defaultMessage='Liste filtern'
+              id="filter.filterButtonTitle"
+              description="Title of the Filter Menu Toggle-Button"
+              defaultMessage="Liste filtern"
             />
           </button>
         </div>
       </header>
     );
   }
-
 }
 
-export default connect(state => {
-  let filtersActive = false;
-  const activeFilters = state.app.get('activeFilters');
-  const categories = state.app.get('categories');
+export default connect(
+  (state) => {
+    let filtersActive = false;
+    const activeFilters = state.app.get("activeFilters");
+    const searchFilter = state.app.get("searchFilter");
+    const categories = state.app.get("categories");
 
-  if (activeFilters.size > 0) filtersActive = true;
-  if (categories.find((c) => !c.get('show'))) filtersActive = true;
+    if (activeFilters.size > 0) filtersActive = true;
+    if (categories.find((c) => !c.get("show"))) filtersActive = true;
 
-  return {
-    showFilterMenu: state.menu.get('showFilterMenu'),
-    filtersActive,
-  };
-}, null, null)(FilterTitle);
+    return {
+      showFilterMenu: state.menu.get("showFilterMenu"),
+      filtersActive,
+      searchFilter,
+    };
+  },
+  null,
+  null
+)(FilterTitle);
